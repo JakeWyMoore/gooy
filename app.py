be1774bd775dbf6be1e519ada3a0b1c76202d8f7
Jacob Moore <jakewymoore@gmail.com>
2021-11-13 19:16:47 -0700

    final commit

--- app.py
+++ app.py
@@ -37,10 +37,14 @@ frame = tk.Frame(root, bg = "white")
 frame.place(relwidth = 0.8, relheight = 0.8, relx = 0.1, rely = 0.1)
 
 openFile = tk.Button(root, text = "Open File", padx = 10, pady = 5, fg = "white", bg = "#263D42", command = addApp)
-runApps = tk.Button(root, text = "Run Apps", padx = 10, pady = 5, fg = "white", bg = "#263D42", command = runApps)
-
 openFile.pack()
+
+runApps = tk.Button(root, text = "Run Apps", padx = 10, pady = 5, fg = "white", bg = "#263D42", command = runApps)
 runApps.pack()
+
+for app in apps:
+    label = tk.Label(frame, text = app)
+    label.pack()
 
 root.mainloop()
 
